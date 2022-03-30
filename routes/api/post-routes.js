0574fc732ea6328d8118182df073b3fea0562aef
jambrose0 <jambrose0518@gmail.com>
2022-03-29 20:26:24 -0400

    upvote function works and cleaned up redundanct code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
+const { sequelize } = require("../../models/User");
 
 //get all
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    attributes: ["id", "post_ur", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_ur",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -27,17 +39,34 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
-        model: User,
-        attributes: ["username"],
+        model: Post,
+        attributes: ["id", "title", "post_url", "created_at"],
+      },
+      {
+        model: Post,
+        attributes: ["title"],
+        through: Vote,
+        as: "voted_posts",
       },
     ],
   })
     .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).json({ message: "No poist found with this id" });
+        res.status(404).json({ message: "No post found with this id" });
         return;
       }
       res.json(dbPostData);
@@ -59,6 +88,15 @@ router.post("/", (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
+    });
+});
+//upvote posts
+router.put("/upvote", (req, res) => {
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
